d11ff28885ecb8cb4e7577fb68d5d3fdbf5c88ad
LuminitaIon <andreea_ion63@yahoo.com>
2022-11-01 16:21:09 +0200

    app bar

---
+++ taskly/lib/pages/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage();
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late double _deviceHeight, _deviceWidth;
+
+  _HomePageState();
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight * 0.15,
+        title: const Text(
+          "Taskly!",
+          style: TextStyle(
+            fontSize: 25,
+            ),
+          ),
+      ),
+    );
+  }
+}
