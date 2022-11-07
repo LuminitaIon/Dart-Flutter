1944f62e6f512e6cddf5c0ff934e7698bef7aa1a
LuminitaIon <andreea_ion63@yahoo.com>
2022-11-07 14:13:03 +0200

    tasks updates, delete a task, add a task

--- taskly/lib/pages/home_page.dart
+++ taskly/lib/pages/home_page.dart
@@ -2,6 +2,8 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:taskly/models/task.dart';
 
 class HomePage extends StatefulWidget {
   HomePage();
@@ -13,7 +15,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late double _deviceHeight, _deviceWidth;
 
+  String? _newTaskContent;
+
+  Box? _box;
+
   _HomePageState();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,40 +40,103 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
       ),
-      body: _tasksList(),
+      body: _tasksView(),
       floatingActionButton: _addTaskButton(),
+    );
+  }
+
+  Widget _tasksView() {
+    return FutureBuilder(
+      future: Hive.openBox('tasks'),
+      builder: (BuildContext _context, AsyncSnapshot _snapshot) {
+        if(_snapshot.hasData) {
+          _box = _snapshot.data;
+          return _tasksList();
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
     );
   }
 
 
   Widget _tasksList() {
-    return ListView(
-      children: [
-        ListTile(
-          title: const Text(
-            'Do laundry!',
+    List tasks = _box!.values.toList(); 
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (BuildContext _context, int _index) {
+        var task = Task.fromMap(tasks[_index]);
+        return ListTile(
+          title: Text(
+            task.content,
             style: TextStyle(
-              decoration: TextDecoration.lineThrough,
+              decoration: task.done ? TextDecoration.lineThrough : null,
             ),
           ),
           subtitle: Text(
-            DateTime.now().toString(),
+            task.timestamp.toString(),
           ),
-          trailing: const Icon(
-            Icons.check_box_outlined,
+          trailing: Icon(
+            task.done ? Icons.check_box_outlined : Icons.check_box_outline_blank_outlined,
             color: Colors.red,
           ),
-        ),
-      ],
+          onTap: () {
+            setState(() {
+              task.done = !task.done;
+              _box!.putAt(
+                _index,
+                task.toMap(),
+              );
+            });
+          },
+          onLongPress: () {
+            setState(() {
+              _box!.deleteAt(_index);
+            });
+          },
+        );
+      },
     );
+
   }
 
   Widget _addTaskButton() {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: _displayTaskPopup,
       child: const Icon(
         Icons.add,
       ),
     );
+  }
+
+  void _displayTaskPopup() {
+    showDialog(context: context, builder: (BuildContext _context) {
+      return AlertDialog(
+        title: const Text("Add New Task!"),
+        content: TextField(
+          onSubmitted: (_) {
+            if(_newTaskContent != null) {
+              var _task = Task(
+                content: _newTaskContent!,
+                timestamp: DateTime.now(),
+                done: false,
+              );
+              _box!.add(_task.toMap());
+              setState(() {
+                _newTaskContent = null;
+                Navigator.pop(context);
+              });
+            }
+          },
+          onChanged: (_value) {
+            setState(() {
+              _newTaskContent = _value;
+            });
+          },
+        ),
+      );
+    });
   }
 }
